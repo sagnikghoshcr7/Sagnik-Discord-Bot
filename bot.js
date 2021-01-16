a97b5a217ea6f96b990d84c1f4f365a273f8bcc2
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-16 18:29:23 +0530

    some modifications

--- bot.js
+++ bot.js
@@ -5,14 +5,16 @@ client.login('Nzk5OTY4NjkzMjE1NjkwODAz.YALS5w.bul-hGVoGrTFzmRpeO7cscWbuCs');
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
-    console.log('❤');
+    console.log('Connected');
 }
 
 client.on('message', gotMessage);
 
 function gotMessage(msg) {
     console.log(msg.content);
+    // if (msg,channel.id == 'CHANNEL ID' && msg.content == 'Hi') {
     if (msg.content == 'Hi') {
+        msg.channel.send('Hello from Sagnik ❤');
         msg.reply('Hello from Sagnik ❤');
     }
 }
