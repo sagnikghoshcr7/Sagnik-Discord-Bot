930843ceadcd7d8b562e2c47707b3767ad0ef105
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-16 21:08:17 +0530

    modified

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const fetch = require('node-fetch');
+
 const  Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOTTOKEN);
@@ -27,11 +29,12 @@ async function gotMessage(msg) {
         const index = Math.floor((Math.random() * replies.length));
         msg.reply(replies[index]);
     } else if (msg.content == '!gif') {
-        msg.channel.send("gif!");
+        // msg.channel.send("gif!");
 
         let url = `https://api.tenor.com/v1/search?q=sagnik&key=${process.env.TENORKEY}&limit=8`
         let response = await fetch(url);
         let json = await response.json();
-        console.log(json);
+        const index = Math.floor((Math.random() * json.results.length));
+        msg.channel.send(json.results[index].url);
     }
 }
