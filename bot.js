f26ffca75793e548ca1752bddf1d33adfc269af1
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-16 18:07:18 +0530

    testing

--- bot.js
+++ bot.js
@@ -1,2 +1,18 @@
 const  Discord = require('discord.js');
 const client = new Discord.Client();
+client.login('Nzk5OTY4NjkzMjE1NjkwODAz.YALS5w.bul-hGVoGrTFzmRpeO7cscWbuCs');
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log('❤');
+}
+
+client.on('message', gotMessage);
+
+function gotMessage(msg) {
+    console.log(msg.content);
+    if (msg.content == 'Hi') {
+        msg.reply('Hello');
+    }
+}
