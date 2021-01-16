0f121874f953147085b9b5a94a84d7e8ae512200
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-01-16 18:47:11 +0530

    add variable messages

--- bot.js
+++ bot.js
@@ -8,13 +8,21 @@ function readyDiscord() {
     console.log('Connected');
 }
 
+const replies = [
+    'Hello 💖',
+    'Hello Bro 💖',
+    'Hello from Sagnik💖',
+    'Hello Love 💙'
+]
+
 client.on('message', gotMessage);
 
 function gotMessage(msg) {
     console.log(msg.content);
     // if (msg,channel.id == 'CHANNEL ID' && msg.content == 'Hi') {
     if (msg.content == 'Hi') {
-        msg.channel.send('Hello from Sagnik ❤');
-        msg.reply('Hello from Sagnik ❤');
+        // msg.channel.send('Hello from Sagnik ❤');
+        const index = Math.floor((Math.random() * replies.length));
+        msg.reply(replies[index]);
     }
 }
